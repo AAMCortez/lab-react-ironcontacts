d5fdf5642bc451227c5ace1a08e9e84a133ea096
António Cortez <aamcortez@gmail.com>
2023-01-21 14:30:56 +0000

    LAB DONE

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import contacts from "./contacts.json";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+   const [fiveContacts, setContacts] = useState(contacts.slice(0, 4));
+   function hasOscar(contact) {
+      if (contact.wonOscar === true) {
+         return <td>🏆</td>;
+      } else {
+         <td> </td>;
+      }
+   }
+   function hasEmmy(contact) {
+      if (contact.wonEmmy === true) {
+         return <td>🏆</td>;
+      } else {
+         <td> </td>;
+      }
+   }
+   function handleAddContact() {
+      const randomContact =
+         contacts[Math.floor(Math.random() * contacts.length)];
+      setContacts([...fiveContacts, randomContact]);
+   }
 
+   function handleSortByName() {
+      const sortedName = fiveContacts.sort((a, b) =>
+         a.name.localeCompare(b.name)
+      );
+      return setContacts(() => [...sortedName]);
+   }
+   function handleSortByPopularity() {
+      const sortedPop = fiveContacts.sort(
+         (a, b) => b.popularity - a.popularity
+      );
+      return setContacts(() => [...sortedPop]);
+   }
+
+   function handleDeleteContact(contactId) {
+      const filteredContacts = [...fiveContacts].filter((contact) => {
+         return contact.id !== contactId;
+      });
+      return setContacts(filteredContacts);
+   }
+   return (
+      <div className="App">
+         <h1>IronContacts</h1>
+         <div>
+            <button onClick={handleAddContact}>Add Random Contact</button>
+            <button onClick={handleSortByName}>Sort by Name</button>
+            <button onClick={handleSortByPopularity}>Sort by Popularity</button>
+         </div>
+         <table>
+            <tr>
+               <th>Picture</th>
+               <th>Name</th>
+               <th>Popularity</th>
+               <th>Won Oscar</th>
+               <th>Won Emmy</th>
+               <th>Action</th>
+            </tr>
+
+            {fiveContacts.map((contact) => {
+               return (
+                  <tr key={contact.id}>
+                     <td>
+                        <img
+                           src={contact.pictureUrl}
+                           width="100px"
+                           height="140px"
+                           alt="contact"
+                        ></img>
+                     </td>
+                     <td>{contact.name}</td>
+                     <td>{Math.floor(contact.popularity * 100) / 100}</td>
+                     {hasOscar(contact)}
+                     {hasEmmy(contact)}
+                     <td>
+                        <button onClick={() => handleDeleteContact(contact.id)}>Delete</button>
+                     </td>
+                  </tr>
+               );
+            })}
+         </table>
+      </div>
+   );
+}
 export default App;
